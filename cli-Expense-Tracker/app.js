e5840a35e6d35f75d3ddf51465b21f91c7a9ac25
qasimjalal4 <qjalal2005@gmail.com>
2026-08-11 14:28:08 +0500

    created a folder and file path

---
+++ cli-Expense-Tracker/app.js
@@ -0,0 +1,11 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const folderPath = path.join(__dirname,'data')
+
+const filePath = path.join(folderPath,'expenses.json')
