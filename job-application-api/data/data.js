53e0cad92586fc4bf324e1a7b6667e71df92e743
qasimjalal4 <qjalal2005@gmail.com>
2026-09-10 14:04:25 +0500

    add job filtering with company and availability

---
+++ job-application-api/data/data.js
@@ -0,0 +1,16 @@
+export const jobs = [
+  {
+    id: 1,
+    company: "TechCorp",
+    position: "Junior Backend Developer",
+    available: true
+  },
+  {
+    id: 2,
+    company: "DevSolutions",
+    position: "Frontend Developer",
+    available: true
+  }
+]
+
+export const applications = []
--- job-application-api/server.js
+++ job-application-api/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { jobs, applications } from './data/data'
 
 const app = express()
 const PORT = 3000
@@ -8,6 +9,32 @@ app.get('/', (req,res) => {
   res.status(200).json({
     success: true,
     message: 'Job Application API is running!'
+  })
+})
+
+
+app.get('/api/jobs', (req,res) => {
+
+  const { available, company } = req.query
+
+  let result = jobs
+
+  if(available === 'true') {
+    result =  result.filter(job => job.available === true)
+  }
+
+  if(company) {
+    result = result.filter(job => job.company === company)
+  }
+
+  if(company && available === 'true') {
+    result = result.filter(job => job.company === company && job.available === true)
+  }
+
+
+  res.status(200).json({
+    success: true,
+    data: result
   })
 })
 
