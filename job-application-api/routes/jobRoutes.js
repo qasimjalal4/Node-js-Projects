4c80f1400114cf06ff476ca1dbf9f8ccc64cca2e
qasimjalal4 <qjalal2005@gmail.com>
2026-09-10 21:33:26 +0500

    move job routes to Express Router

---
+++ job-application-api/routes/jobRoutes.js
@@ -0,0 +1,189 @@
+import express from 'express'
+
+
+const router = express.Router()
+
+router.get('/', (req,res) => {
+
+  const { available, company } = req.query
+
+  let result = jobs
+
+  if(available === 'true') {
+    result =  result.filter(job => job.available === true)
+  }
+
+  if(company) {
+    result = result.filter(job => job.company === company)
+  }
+
+  if(company && available === 'true') {
+    result = result.filter(job => job.company === company && job.available === true)
+  }
+
+
+  res.status(200).json({
+    success: true,
+    data: result
+  })
+})
+
+
+router.get('/:id', (req,res) => {
+
+  const id = Number(req.params.id)
+
+  const job = jobs.find(job => job.id === id)
+
+  if(!job) {
+    return res.status(404).json({
+      success: false,
+      message: 'Job not found!'
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    data: job
+  })
+})
+
+
+
+router.post('/', (req, res) => {
+
+  const { company, position, available } = req.body
+
+  
+  if (!company || !company.trim()) {
+    return res.status(400).json({
+      success: false,
+      message: "Company is required"
+    })
+  }
+
+  if (!position || !position.trim()) {
+    return res.status(400).json({
+      success: false,
+      message: "Position is required"
+    })
+  }
+
+  const newJob = {
+    id: jobs.length > 0 ? Math.max(...jobs.map(job => job.id)) + 1 : 1,
+    company,
+    position,
+    available
+  }
+
+
+  jobs.push(newJob)
+
+  res.status(201).json({
+    success: true,
+    data: jobs
+  })
+})
+
+
+
+router.patch('/:id', (req, res) => {
+
+  const id = Number(req.params.id)
+
+  const job = jobs.find(job => job.id === id)
+
+  if(!job) {
+    res.status(404).json({
+      success: false,
+      message: 'Job not found!'
+    })
+  }
+
+  const { company, position, available} = req.body
+
+  if(company !== undefined) {
+    if(!company.trim()) {
+      res.status(400).json({
+        success: false,
+        message: 'Company cannot be empty!'
+      })
+    }
+
+    job.company = company.trim()
+
+  }
+
+  if(position !== undefined) {
+    if(!position.trim()) {
+      res.status(400).json({
+        success: false,
+        message: 'Position cannot be empty!'
+      })
+    }
+
+    job.position = position.trim()
+  }
+
+
+  if(available !== undefined) {
+    job.available = true
+  }
+
+
+  res.status(200).json({
+      success: true,
+      data: job
+  })
+
+})
+
+
+
+router.delete('/:id', (req,res) => {
+
+  const id = Number(req.params.id)
+
+  const jobExists = jobs.some(job => job.id === id)
+
+  if(!jobExists) {
+    return res.status(404).json({
+      success: false,
+      message: 'Job not found!'
+    })
+  }
+
+  const filteredJobs = jobs.filter(job => job.id !== id)
+
+  jobs.length = 0
+  jobs.push(...filteredJobs)
+
+  res.status(200).json({
+    success: true,
+    message: 'Job deleted successfully!'
+  })
+
+
+
+  /*
+
+  const jobIndex = jobs.findIndex(job => job.id === id)
+
+  if (jobIndex === -1) {
+    return res.status(404).json({
+      success: false,
+      message: 'Job not found!'
+    })
+  }
+
+
+  jobs.splice(jobIndex, 1)
+
+
+
+  */
+})
+
+
+
+export default router
--- job-application-api/server.js
+++ job-application-api/server.js
@@ -1,10 +1,12 @@
-import express, { application } from 'express'
+import express from 'express'
 import { jobs, applications } from './data/data'
+import jobRouter from './routes/jobRoutes'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+
 
 app.get('/', (req,res) => {
   res.status(200).json({
@@ -14,185 +16,17 @@ app.get('/', (req,res) => {
 })
 
 
-app.get('/api/jobs', (req,res) => {
+app.use('/api/jobs', jobRouter)
 
-  const { available, company } = req.query
+app.use('/api/jobs/:id', jobRouter)
 
-  let result = jobs
+app.use('/api/jobs', jobRouter)
 
-  if(available === 'true') {
-    result =  result.filter(job => job.available === true)
-  }
-
-  if(company) {
-    result = result.filter(job => job.company === company)
-  }
-
-  if(company && available === 'true') {
-    result = result.filter(job => job.company === company && job.available === true)
-  }
-
-
-  res.status(200).json({
-    success: true,
-    data: result
-  })
-})
+app.use('/api/jobs/:id', jobRouter)
 
 
 
-app.get('/api/jobs/:id', (req,res) => {
-
-  const id = Number(req.params.id)
-
-  const job = jobs.find(job => job.id === id)
-
-  if(!job) {
-    return res.status(404).json({
-      success: false,
-      message: 'Job not found!'
-    })
-  }
-
-  res.status(200).json({
-    success: true,
-    data: job
-  })
-})
-
-
-app.post('/api/jobs', (req, res) => {
-
-  const { company, position, available } = req.body
-
-  
-  if (!company || !company.trim()) {
-    return res.status(400).json({
-      success: false,
-      message: "Company is required"
-    })
-  }
-
-  if (!position || !position.trim()) {
-    return res.status(400).json({
-      success: false,
-      message: "Position is required"
-    })
-  }
-
-  const newJob = {
-    id: jobs.length > 0 ? Math.max(...jobs.map(job => job.id)) + 1 : 1,
-    company,
-    position,
-    available
-  }
-
-
-  jobs.push(newJob)
-
-  res.status(201).json({
-    success: true,
-    data: jobs
-  })
-})
-
-
-app.patch('/api/jobs/:id', (req, res) => {
-
-  const id = Number(req.params.id)
-
-  const job = jobs.find(job => job.id === id)
-
-  if(!job) {
-    res.status(404).json({
-      success: false,
-      message: 'Job not found!'
-    })
-  }
-
-  const { company, position, available} = req.body
-
-  if(company !== undefined) {
-    if(!company.trim()) {
-      res.status(400).json({
-        success: false,
-        message: 'Company cannot be empty!'
-      })
-    }
-
-    job.company = company.trim()
-
-  }
-
-  if(position !== undefined) {
-    if(!position.trim()) {
-      res.status(400).json({
-        success: false,
-        message: 'Position cannot be empty!'
-      })
-    }
-
-    job.position = position.trim()
-  }
-
-
-  if(available !== undefined) {
-    job.available = true
-  }
-
-
-  res.status(200).json({
-      success: true,
-      data: job
-  })
-
-})
-
-
-
-app.delete('/api/jobs/:id', (req,res) => {
-
-  const id = Number(req.params.id)
-
-  const jobExists = jobs.some(job => job.id === id)
-
-  if(!jobExists) {
-    return res.status(404).json({
-      success: false,
-      message: 'Job not found!'
-    })
-  }
-
-  const filteredJobs = jobs.filter(job => job.id !== id)
-
-  jobs.length = 0
-  jobs.push(...filteredJobs)
-
-  res.status(200).json({
-    success: true,
-    message: 'Job deleted successfully!'
-  })
-
-
-
-  /*
-
-  const jobIndex = jobs.findIndex(job => job.id === id)
-
-  if (jobIndex === -1) {
-    return res.status(404).json({
-      success: false,
-      message: 'Job not found!'
-    })
-  }
-
-
-  jobs.splice(jobIndex, 1)
-
-
-
-  */
-})
+app.use('/api/jobs/:id', jobRouter)
 
 
 
