a116dbfee948a37587dcd3c081ef1f34c06d299c
qasimjalal4 <qjalal2005@gmail.com>
2026-09-10 22:44:41 +0500

    move application logic to controllers

---
+++ job-application-api/controllers/applicationControllers.js
@@ -0,0 +1,190 @@
+import { jobs, applications } from "../data/data";
+
+
+
+
+export const getApplications = (req,res) => {
+
+
+  const { status, jobId } = req.query
+
+  let result = applications
+
+  if (status === 'applied') {
+    result = result.filter(application => application.status === 'applied')
+  }
+
+  if (status === 'withdrawn') {
+    result = result.filter(application => application.status === 'withdrawn')
+  }
+
+
+  if (jobId) {
+    result = result.filter(
+      application => application.jobId === Number(jobId)
+    )
+  }
+
+
+  res.status(200).json({
+    success: true,
+    data: result
+  })
+}
+
+
+
+
+export const getApplicationById = (req,res) => {
+
+  const id = Number(req.params.id)
+
+  const application = applications.find(application => application.id === id)
+
+  if(!application) {
+    return res.status(404).json({
+      success: false,
+      message: 'Application not found!'
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    data: application
+  })
+}
+
+
+
+
+
+export const createApplication = (req, res) => {
+
+  const { jobId, applicantName } = req.body
+
+  if(!applicantName.trim()) {
+    return res.status(400).json({
+      success: false,
+      message: 'Applicant name is required!'
+    })
+  }
+
+  const job = jobs.find(job => job.id === jobId)
+
+  if(!job) {
+    return res.status(404).json({
+      success: false,
+      message: 'Job not found!'
+    })
+  }
+
+  if(!job.available) {
+    return res.status(409).json({
+      success: false,
+      message: 'This job is no longer available'
+    })
+  }
+
+    const alreadyApplied = applications.some(
+    application =>
+      application.jobId === Number(jobId) &&
+      application.applicantName === applicantName.trim()
+  )
+
+  if (alreadyApplied) {
+    return res.status(409).json({
+      success: false,
+      message: 'You have already applied to this job!'
+    })
+  }
+
+
+
+
+  const newApplication = {
+    id: applications > 0 ? Math.max(...applications.map(application => application.id)) + 1 : 1,
+    jobId,
+    applicantName,
+    status: 'applied'
+  }
+
+
+  applications.push(newApplication)
+
+
+  res.status(201).json({
+    success: true,
+    message: 'Applied successfully!'
+  })
+}
+
+
+
+
+
+export const updateApplication = (req, res) => {
+  const id = Number(req.params.id)
+
+  const application = applications.find(
+    application => application.id === id
+  )
+
+  // Application doesn't exist
+  if (!application) {
+    return res.status(404).json({
+      success: false,
+      message: 'Application not found!'
+    })
+  }
+
+  const { status } = req.body
+
+  // Only allow "withdrawn"
+  if (status !== 'withdrawn') {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid status!'
+    })
+  }
+
+  // Already withdrawn
+  if (application.status === 'withdrawn') {
+    return res.status(409).json({
+      success: false,
+      message: 'Application is already withdrawn!'
+    })
+  }
+
+  // Update status
+  application.status = 'withdrawn'
+
+  return res.status(200).json({
+    success: true,
+    data: application
+  })
+}
+
+
+
+
+export const deleteApplication = (req, res) => {
+  const id = Number(req.params.id)
+
+  const applicationIndex = applications.findIndex(
+    application => application.id === id
+  )
+
+  if (applicationIndex === -1) {
+    return res.status(404).json({
+      success: false,
+      message: 'Application not found!'
+    })
+  }
+
+  applications.splice(applicationIndex, 1)
+
+  return res.status(200).json({
+    success: true,
+    message: 'Application deleted successfully'
+  })
+}
