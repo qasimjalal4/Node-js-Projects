305eaa73a4b66f47111f3a057f85f1f71e1719ac
qasimjalal4 <qjalal2005@gmail.com>
2026-09-11 21:12:50 +0500

    created custom errors

---
+++ job-application-api/errors/BadRequestError.js
@@ -0,0 +1,16 @@
+import AppError from "./AppError";
+
+
+
+class BadRequestError extends AppError {
+
+  constructor(message) {
+
+    super(message,400)
+  }
+}
+
+
+
+
+export default BadRequestError
---
+++ job-application-api/errors/NotFoundError.js
@@ -0,0 +1,13 @@
+import AppError from "./AppError";
+
+
+class NotFoundError extends AppError {
+
+  constructor(message) {
+
+    super(message,404)
+  }
+}
+
+
+export default NotFoundError
